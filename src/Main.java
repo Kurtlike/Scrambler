8f70ef91da4ec871f8222fa3aa30a687411db6b0
kurtlike <gordonfriman2000@gmail.com>
2022-02-08 12:41:56 +0300

    some fixes

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         ArrayList<Integer> outBin = Scrambler.scramb(Parser.parseBinaryString(args[0]), Parser.parseBinaryString(Arrays.copyOfRange(args,1,args.length)));
         for(int i = 0; i < outBin.size(); i ++){
             System.out.print(outBin.get(i));
-            if(i % 7 == 0 && i != 0) System.out.print(" ");
+            if((i + 1) % 8 == 0) System.out.print(" ");
         }
     }
 
